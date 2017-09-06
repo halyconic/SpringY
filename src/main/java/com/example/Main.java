22fe9a3c157a69f29067e230aed99596ae2ab9fe
Stephen Webber <webber.sl@gmail.com>
2017-09-05 22:48:07 -0500

    first commit - index.html still needs work

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,88 +1,68 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
+/*
+ * This application is designed to explore the Spring Boot framework.
+ * 
+ * Done: Explore template conventions
+ * 
+ * Done: Host static resource
+ * 
+ * Done: Integrate with Heroku and Github
+ *  
+ * TODO: Add unit tests
+ * 
+ * TODO: Add a dynamic component supported by templates
+ * 
+ * TODO: Add a database component
+ */
 @Controller
 @SpringBootApplication
-public class Main {
+public class Main {  
+    @Autowired
+    ServletContext servletContext;
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Autowired
-  private DataSource dataSource;
-
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
-  }
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
+    @RequestMapping("/")
+    String index() {
+        return "index";
     }
-  }
 
+    /* 
+     * https://stackoverflow.com/questions/5673260/downloading-a-file-from-spring-controllers
+     * 
+     * TODO: The second answer here feels more idiomatic, but didn't work.
+     * Perhaps after learning more this will become the better option
+     * 
+     * The pdf seems to be taking a while to load - perhaps it is loading into memory first
+     */
+    @RequestMapping(value = "/resume", method = RequestMethod.GET)
+    public void getFile(HttpServletResponse response) {
+        try {
+            // TODO: don't hardcode src/main/resources here
+            InputStream inputStream = new FileInputStream("src/main/resources/resume.pdf");
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+            response.setContentType("application/pdf");
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
